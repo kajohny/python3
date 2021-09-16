6d1b6bbac6cbf1d02614b93293216dd9b9f87e9a
kajohny <80178491+kajohny@users.noreply.github.com>
2021-09-16 13:20:53 +0600

    Create first_task.py

---
+++ first_task.py
@@ -0,0 +1,10 @@
+ef func(input_char, i = 0):
+    if i == len(input_char):
+        print("".join(input_char))
+    for j in range(i, len(input_char)):
+        string = [char for char in input_char]
+        string[i], string[j] = string[j], string[i]
+        func(string, i + 1)
+print("Enter the symbols: ")
+s = input()
+print(func(s))
